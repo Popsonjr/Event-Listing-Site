e46b6f4df1bfa3841ff48195d591ffe19ebd6e10
Muhammad Popoola <popoolama@gmail.com>
2021-08-11 11:52:09 +0100

    Base components implementation

---
+++ src/components/BaseCheckbox.vue
@@ -0,0 +1,26 @@
+<template>
+  <input
+    type="checkbox"
+    :checked="modelValue"
+    @change="$emit('update:modelValue', $event.target.checked)"
+    v-bind="$attrs"
+  />
+  <label v-if="label">{{ label }}</label>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: Boolean,
+      default: true,
+    },
+  },
+}
+</script>
+
+<style scoped></style>
